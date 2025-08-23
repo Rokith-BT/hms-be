a9c4b6257db5c484b767d7f770ad887c86fea39b
Rokith-BT <145829153+Rokith-BT@users.noreply.github.com>
2025-08-23 10:16:03 +0530

    Update appointment_filter.module.ts

--- src/appointment_filter/appointment_filter.module.ts
+++ src/appointment_filter/appointment_filter.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppointmentFilterService } from './appointment_filter.service';
 import { AppointmentFilterController } from './appointment_filter.controller';
 
-@Module({
-  controllers: [AppointmentFilterController],
-  providers: [AppointmentFilterService],
+
 })
 export class AppointmentFilterModule {}
